fe8e42dff0254d8d3a69c8d4fba6bb6ac9428b4b
mitrofin <mitrofn@gmail.com>
2020-10-22 23:52:08 +0300

    Updates

--- src.799e86ee.js
+++ src.799e86ee.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./css/styles.css"),console.log("Мастерская: деплой билда от Parcel на GitHub Pages");
 },{"./css/styles.css":"krre"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-10-food-service/src.a7961fa7.js.map
+//# sourceMappingURL=/goit-js-hw-10-food-service/src.799e86ee.js.map
