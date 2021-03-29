438cbfdef60fb27252edf283933f166e725a255c
mitrofin <43621297+mitrofin@users.noreply.github.com>
2021-03-29 21:51:03 +0300

    Update change-theme.js

--- src/js/change-theme.js
+++ src/js/change-theme.js
@@ -5,25 +5,23 @@ const Theme = {
 
 const bodyEl = document.body;
 const changeEl = document.getElementById('theme-switch-toggle');
-const savedTheme = localStorage.getItem('Theme', `${Theme.LIGHT}`);
-changeEl.addEventListener('change', onChangeClick);
+const savedTheme = localStorage.getItem('Theme') || Theme.LIGHT;
 
+changeEl.addEventListener('change', onChangeClick);
 
 bodyEl.classList.add(savedTheme);
 
-if (savedTheme === `${Theme.DARK}`) {
-  changeEl.checked = true
-}
+changeEl.checked = savedTheme === Theme.DARK;
 
 function onChangeClick(e) {
   if (e.target.checked) {
-    bodyEl.classList.add(`${Theme.DARK}`);
-    bodyEl.classList.remove(`${Theme.LIGHT}`);
-    localStorage.setItem('Theme', `${Theme.DARK}`);
+    bodyEl.classList.add(Theme.DARK);
+    bodyEl.classList.remove(Theme.LIGHT);
+    localStorage.setItem('Theme', Theme.DARK);
   }
   else {
-    bodyEl.classList.add(`${Theme.LIGHT}`);
-    bodyEl.classList.remove(`${Theme.DARK}`);
-    localStorage.setItem('Theme', `${Theme.LIGHT}`);
+    bodyEl.classList.add(Theme.LIGHT);
+    bodyEl.classList.remove(Theme.DARK);
+    localStorage.setItem('Theme', Theme.LIGHT);
   }
 }
